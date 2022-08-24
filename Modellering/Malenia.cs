5ded86bd2cada138a4d76d21ce2a808024c56aa5
Paul Chastain <paulchast1@gmail.com>
2022-08-24 13:46:32 +0200

    finnished

---
+++ Modellering/Malenia.cs
@@ -0,0 +1,31 @@
+public class Malenia
+{
+    int HP;
+    int damage;
+    double x, y, z;
+    double spawnX, spawnY, spawnZ;
+    double rotation;
+
+
+    string[] phases = new string[] {
+        "Invincible",
+        "Damageable"
+    };
+
+    string[] strengths = new string[] {
+        "Stuff",
+        "Something",
+        "Idk"
+    };
+
+    string[] weaknesses = new string[] {
+        "Bob",
+        "Bill",
+        "Barney"
+    };
+
+    string[] lootDrops = new string[] {
+        "Gold",
+        "Sword"
+    };
+}
---
+++ Modellering/Maliketh.cs
@@ -0,0 +1,25 @@
+public class Maliketh
+{
+    int HP;
+    int damage;
+    double x, y, z;
+    double rotation;
+
+
+    string[] strengths = new string[] {
+        "Stuff",
+        "Something",
+        "Idk"
+    };
+
+    string[] weaknesses = new string[] {
+        "Bob",
+        "Bill",
+        "Barney"
+    };
+
+    string[] lootDrops = new string[] {
+        "Gold",
+        "Sword"
+    };
+}
